ff57bc1ae0f77bcc83c30a97c2e2bc9b382f78e2
Adiel30 <52074529+Adiel30@users.noreply.github.com>
2020-03-31 19:56:10 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-tflint_vars=$1
+tflint_vars=./terragrunt.hcl
+prototypes_folder=`cat $tflint_vars|grep source |sed 's| ||g'|sed 's|prototypes/| |' |awk '{print $2}' |sed 's|"||'`
 input_variables_line_count=`grep -E "input.+=.+{" -A100 $tflint_vars |grep -E -m1 "^}$" -B 100 |wc -l`
 tail=`echo "aasd"|awk "{print($input_variables_line_count-1)}"`
 head=`echo "aasd"|awk "{print($input_variables_line_count-2)}"`
-grep -E "input.+=.+{" -A100 $tflint_vars |grep -E -m1 "^}$" -B 100 |tail -n $tail|head -n $head
+inputs=`grep -E "input.+=.+{" -A100 $tflint_vars |grep -E -m1 "^}$" -B 100 |tail -n $tail|head -n $head`
 
-
+echo $inputs > ../../prototypes/$prototypes_folder/tflint.tfvars
